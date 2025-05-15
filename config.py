7bf6b43e1ec1a86e88b91cc5bad098fac0534d99
Nicholas Afonin <33298153+nick-afonin@users.noreply.github.com>
2025-05-14 23:13:10 -0400

    adjusted some paths

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 
 
 class path:
-    raw_csv_directory = "/scratch/a/alim/afoninni/MUSE/MUSE-PSG/"
-    synced_csv_directory = "/scratch/a/alim/afoninni/MUSE/muse_synced/"
-    ACC_features_directory = "/scratch/a/alim/afoninni/MUSE/ACC_features/"
-    EEG_features_directory = "/scratch/a/alim/afoninni/MUSE/EEG_features/"
-    combined_features = "/scratch/a/alim/afoninni/MUSE/ACC_EEG_features/"
-    labelled_features = "/scratch/a/alim/afoninni/MUSE/labelled_features/"
+    raw_csv_directory = "/scratch/a/alim/afoninni/muse/MUSE-PSG/"
+    synced_csv_directory = "/scratch/a/alim/afoninni/muse/muse_synced/"
+    ACC_features_directory = "/scratch/a/alim/afoninni/muse/ACC_features/"
+    EEG_features_directory = "/scratch/a/alim/afoninni/muse/EEG_features/"
+    combined_features = "/scratch/a/alim/afoninni/muse/ACC_EEG_features/"
+    labelled_features = "/scratch/a/alim/afoninni/muse/labelled_features/"
