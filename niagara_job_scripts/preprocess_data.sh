4123d20247344781a4c2be2c6994d95072909818
Nicholas Afonin <33298153+nick-afonin@users.noreply.github.com>
2025-06-02 21:16:48 -0400

    a

--- niagara_job_scripts/preprocess_data.sh
+++ niagara_job_scripts/preprocess_data.sh
@@ -18,4 +18,4 @@ export NUMBA_NUM_THREADS=1
 
 export PYTHONPATH=..  # Set the working path directory to the project root so files like config can be accessed.
 
-python extract_acc_features.py
+python extract_eeg_features.py
