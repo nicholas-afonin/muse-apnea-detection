677627d161e621b858142ba351c32a84f581c461
Nicholas Afonin <33298153+nick-afonin@users.noreply.github.com>
2025-06-05 14:59:30 -0400

    committing don't remember what I added - slightly concerning i guess we'll find out.

--- deep_learning/apnea_binary_classifier.py
+++ deep_learning/apnea_binary_classifier.py
@@ -17,6 +17,9 @@ from imblearn.over_sampling import SMOTE
 import config
 import random
 import glob
+import os
+import pickle
+import json
 
 
 """ -------------------------------------- HELPER FUNCS -------------------------------------- """
@@ -33,15 +36,18 @@ def combine_files_into_df(csv_path_list):
 """ -------------------------------------- CORE FUNCS -------------------------------------- """
 
 
-def load_data(processed_features_directory, train_test_ratio):
+def load_data(processed_features_directory: str, train_val_test_ratio: (float, float, float)):
     all_csv_file_paths = glob.glob(processed_features_directory + "*.csv")
+
+    # Break down into data proportions
+    train_ratio, val_ratio, test_ratio = train_val_test_ratio
 
     # Shuffle the list (for randomness, but reproducible with a seed)
     random.seed(21)
     random.shuffle(all_csv_file_paths)
 
     # Split into train and test
-    split_index = int(len(all_csv_file_paths) * train_test_ratio)
+    split_index = int(len(all_csv_file_paths) * train_ratio)
     train_files = all_csv_file_paths[:split_index]
     test_files = all_csv_file_paths[split_index:]
 
@@ -59,27 +65,30 @@ def load_data(processed_features_directory, train_test_ratio):
     test_df = pd.DataFrame(scaler.transform(test_df), columns=test_df.columns)
 
     # Split into X and Y train and test
-    X_train = train_df.drop(columns=['ts', 'name', 'arousal_event'])
-    X_test = test_df.drop(columns=['ts', 'name', 'arousal_event'])
+    X_train = train_df.drop(columns=['ts', 'arousal_event'])
+    X_test = test_df.drop(columns=['ts', 'arousal_event'])
 
     y_train = X_train.pop('apnea_event')
     y_test = X_test.pop('apnea_event')
 
     # Create stratified train/validation split
-    # X_train, X_val, y_train, y_val = train_test_split(
-    #     X_train, y_train,
-    #     test_size=0.15,
-    #     stratify=y_train,
-    #     random_state=43
-    # )
-    X_val = X_test  # Validation set NOT USED currently
-    y_val = y_test
+    if val_ratio != 0:
+        X_train, X_val, y_train, y_val = train_test_split(
+            X_train, y_train,
+            test_size=val_ratio/train_ratio,
+            stratify=y_train,
+            random_state=43
+        )
+    else:
+        # IF VALIDATION DATA IS SET UP AS EQUAL TO TEST, BE CAREFUL
+        X_val = X_test
+        y_val = y_test
 
-    # Oversample the minority classes in the training data to give more to train on
+    # Optional - Oversample the minority classes in the training data to give more to train on
     # smote = SMOTE()
     # X_train, y_train = smote.fit_resample(X_train, y_train)
 
-    # Debugging
+    # Optional - Debugging
     # print("Unique labels in y_train:", y_train.unique())
     # print("Any NaNs?", y_train.isna().sum())
     # print(X_train.shape)
@@ -90,14 +99,14 @@ def load_data(processed_features_directory, train_test_ratio):
     return X_train, X_val, X_test, y_train, y_val, y_test
 
 
-def train_model(X_train, y_train, X_val, y_val):
+def train_model(X_train, y_train, X_val, y_val, save_as: str):
     # Create model architecture
     basic_model = Sequential()  # initialize a basic sequential model
-    basic_model.add(Dense(units=128, activation='relu', input_shape=(167,), kernel_regularizer=l2(0.01)))  # input layer
+    basic_model.add(Dense(units=128, activation='relu', input_shape=(168,), kernel_regularizer=l2(0.01)))  # input layer
     basic_model.add(Dropout(0.4))  # to prevent overfitting
-    basic_model.add(Dense(64, activation='tanh', kernel_regularizer=l2(0.01)))
+    basic_model.add(Dense(80, activation='relu', kernel_regularizer=l2(0.01)))
     basic_model.add(Dropout(0.4))
-    basic_model.add(Dense(32, activation='relu', kernel_regularizer=l2(0.01)))
+    basic_model.add(Dense(80, activation='relu', kernel_regularizer=l2(0.01)))
     basic_model.add(Dropout(0.4))
     basic_model.add(Dense(1, activation='sigmoid'))
 
@@ -125,42 +134,67 @@ def train_model(X_train, y_train, X_val, y_val):
         class_weight=class_weights
     )
 
-    return basic_model, history
+    # Save the model to disk
+    directory = os.path.join(config.path.apnea_model_directory, save_as)  # "apnea_model/save_as/"
+    os.makedirs(directory, exist_ok=True)
+    model_save_path = os.path.join(directory, "model.keras")  # "apnea_model/save_as/model.keras"
+    basic_model.save(model_save_path)
+
+    # Save the history to disk
+    with open(os.path.join(directory, "history.pkl"), "wb") as f:  # "apnea-model/save_as/history.pkl"
+        pickle.dump(history.history, f)
 
 
-def evaluate_model(model, history, X_test, y_test, relevant_threshold_value, relevant_window_size):
+def evaluate_model(X_test, y_test, X_val, y_val, load_as: str, relevant_threshold_value, relevant_window_size):
+    # Load model and training history
+    model = load_model(os.path.join(config.path.apnea_model_directory, load_as, "model.keras"))  # "apnea_model/load_as/model.keras"
+    with open(os.path.join(config.path.apnea_model_directory, load_as, "history.pkl"), "rb") as f:  # "apnea_model/load_as/history.pkl"
+        history = pickle.load(f)
+
     # Basic evaluation
     loss_and_metrics = model.evaluate(X_test, y_test)
     print(loss_and_metrics)
     print('Loss = ', loss_and_metrics)
 
     # Plot training & validation loss
-    plt.plot(history.history['loss'], label='Training Loss')
-    plt.plot(history.history['val_loss'], label='Validation Loss')
+    plt.plot(history['loss'], label='Training Loss')
+    plt.plot(history['val_loss'], label='Validation Loss')
     plt.title('Model Loss Over Epochs')
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
     plt.legend()
     plt.grid(True)
-    plt.show()
 
-    # Get predictions, performance metrics, and confusion matrix
-    predicted_probabilities = model.predict(X_test)
-    predicted_probabilities = tf.squeeze(predicted_probabilities)
-    actual = np.array(y_test)
+    # SAVE TRAINING LOSS PLOT
+    save_dir = os.path.join(config.path.apnea_model_directory, load_as)
+    os.makedirs(save_dir, exist_ok=True)
+    loss_plot_path = os.path.join(save_dir, f"training_and_validation_loss.png")
+    plt.savefig(loss_plot_path)
+    plt.close()
 
-    # Identify optimal threshold to use
+    # Get predictions, performance metrics, and confusion matrix FOR VALIDATION
+    val_predicted_probabilities = model.predict(X_val)
+    val_predicted_probabilities = tf.squeeze(val_predicted_probabilities)
+    val_actual = np.array(y_val)
+
+    # Identify optimal threshold to use for considering a model output an apnea prediction (>0.5 confidence means
+    # it's an apnea for example)
     best_f1, best_thresh = 0, 0
     for t in np.linspace(0.1, 0.9, 50):
-        pred = tf.cast(predicted_probabilities > t, tf.int32)
-        score = f1_score(actual, pred)
+        pred = tf.cast(val_predicted_probabilities > t, tf.int32)
+        score = f1_score(val_actual, pred)
         if score > best_f1:
             best_f1, best_thresh = score, t
 
     print("Best Thresh:", best_thresh)
     print("Best F1 Score:", best_f1)
+    best_thresh = 0.50
 
-    # best_thresh = 0.42
+    # Get predictions, performance metrics, and confusion matrix for testing
+    predicted_probabilities = model.predict(X_val)
+    predicted_probabilities = tf.squeeze(predicted_probabilities)
+    actual = np.array(y_val)
+
     predicted = np.array([1 if x >= best_thresh else 0 for x in predicted_probabilities])  # arbitrary threshold
 
     print(classification_report(actual, predicted))  # includes precision, recall, F1
@@ -205,11 +239,48 @@ def evaluate_model(model, history, X_test, y_test, relevant_threshold_value, rel
 
     fig.tight_layout()
     plt.ioff()
-    plt.show()
+
+    # SAVE PLOT instead of displaying
+    save_dir = os.path.join(config.path.apnea_model_directory, load_as)
+    os.makedirs(save_dir, exist_ok=True)
+    plot_path = os.path.join(save_dir, f"confusion_matrix.png")
+    fig.savefig(plot_path)
+    plt.close(fig)
+
+    # Save calculated metrics
+    metrics = {
+        "best_threshold": float(best_thresh),
+        "best_f1": float(best_f1),
+        "roc_auc": float(roc_auc_score(actual, predicted_probabilities)),
+        "mcc": float(matthews_corrcoef(actual, predicted)),
+        "classification_report": classification_report(actual, predicted, output_dict=True)
+    }
+
+    metrics_path = os.path.join(save_dir, f"metrics.json")
+    with open(metrics_path, "w") as f:
+        json.dump(metrics, f, indent=2)
+
+
+def main(threshold, window_size, train_val_test_ratio: (float, float, float), model_name, evaluate_only=False):
+    # Load data in appropriate proportions (randomize and split by files)
+    full_data_path = os.path.join(config.path.EEG_ACC_features_labelled,
+                                  f'EEG_ACC_features_labelled_ApneaThreshold{threshold}_ArousalThreshold{threshold}_window{window_size}/')
+    X_train, X_val, X_test, y_train, y_val, y_test = load_data(full_data_path, train_val_test_ratio)
+
+    # Train the model
+    if not evaluate_only:
+        train_model(X_train, y_train, X_val, y_val, save_as=model_name)
+
+    # Evaluate the model
+    evaluate_model(X_test, y_test, X_val, y_val, load_as=model_name, relevant_threshold_value=threshold,
+                   relevant_window_size=window_size)
 
 
 if __name__ == "__main__":
-    full_data_path = os.path.join(config.path.EEG_ACC_features_labelled, 'EEG_ACC_features_labelled_ApneaThreshold0.01_ArousalThreshold0.01_window30/')
-    X_train, X_val, X_test, y_train, y_val, y_test = load_data(full_data_path, 0.75)
-    model, history = train_model(X_train, y_train, X_test, y_test)
-    evaluate_model(model, history, X_test, y_test, relevant_threshold_value=0.01, relevant_window_size=30)
+    # for thresh in [0.01, 0.25, 0.5, 0.75, 0.95]:
+    #     for window in [1, 5, 10, 15, 20, 25, 30]:
+    #         main(thresh, window, (0.70, 0, 0.15), evaluate_only=False, model_name=f"{thresh}_thresh_{window}s_window")
+
+    window = 30
+    thresh = 0.01
+    main(thresh, window, (0.70, 0, 0.15), evaluate_only=False, model_name=f"{thresh}_thresh_{window}s_window")
