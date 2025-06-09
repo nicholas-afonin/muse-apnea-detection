f9328face6dff35a71d13a368d1403dee91d4337
Nicholas Afonin <33298153+nick-afonin@users.noreply.github.com>
2025-06-09 19:02:39 -0400

    hopefully almost all results are in

--- niagara_job_scripts/run.sh
+++ niagara_job_scripts/run.sh
@@ -4,16 +4,16 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=40
-#SBATCH --output=/scratch/a/alim/afoninni/muse/arousal_grid_search.out
-#SBATCH --error=/scratch/a/alim/afoninni/muse/arousal_grid_search.err
+#SBATCH --output=/scratch/a/alim/afoninni/muse/visualize_search_results.out
+#SBATCH --error=/scratch/a/alim/afoninni/muse/visualize_search_results.err
 
 
 source /home/a/alim/afoninni/.virtualenvs/env3/bin/activate
-cd /home/a/alim/afoninni/muse-apnea-detection/deep_learning || exit
+cd /home/a/alim/afoninni/muse-apnea-detection/diagnostics || exit
 
 export NUMBA_CACHE_DIR=/scratch/a/alim/afoninni/tmp  # fixes an error caused by a sub-dependency (numba)
 export MPLCONFIGDIR=/scratch/a/alim/afoninni/tmp  # also a small fix (this one is not required)
 
 export PYTHONPATH=..  # Set the working path directory to the project root so files like config can be accessed.
 
-python arousal_binary_classifier.py
+python evaluation_helper.py
